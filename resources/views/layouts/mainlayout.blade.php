1e2afb67e369f53a1538f11c5cbafb82e49cb595
MoubarackMagadji <moubjo@gmail.com>
2024-02-29 14:58:30 +0000

    '29020250'

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -3,7 +3,9 @@
 <head>
 	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/bootstrap.css')}}"> 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}"> --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    
     <link href="{{asset('css/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" >
     {{-- <link id="bs-css" href="{{ asset('css/jqD.css') }}" rel="stylesheet"> --}}
     @yield('css')
